4da17bcdf4cb4d7b6c7c9947b7f1efe23d2edf8e
grochalapiotr <miami2042@gmail.com>
2024-11-25 20:28:39 +0100

    feat: adjust system-log service

--- src/core/announcements/announcements.module.ts
+++ src/core/announcements/announcements.module.ts
@@ -16,6 +16,6 @@ import { SystemLogsModule } from '../system-logs/system-logs.module'; // Importu
     SystemLogsModule, // Import SystemLogsModule
   ],
   controllers: [AnnouncementsController],
-  providers: [AnnouncementsService],
+  providers: [AnnouncementsService], // SystemLogsService nie jest już tutaj, bo jest eksportowane przez SystemLogsModule
 })
 export class AnnouncementsModule {}
