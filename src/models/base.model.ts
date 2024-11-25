17d8a67781e635b1a8d80181a7d6ccdf3fadc81e
Michał Woś <m.wos.biznes@gmail.com>
2024-11-25 20:13:51 +0100

    feat: rebased

--- src/models/base.model.ts
+++ src/models/base.model.ts
@@ -10,7 +10,7 @@ export abstract class BaseClass extends BasicClass {
     readonly: true,
     // select: false,
   })
-  createdBy?: string;
+  createdBy?: mongoose.Types.ObjectId;
 
   @Prop({
     required: true,
